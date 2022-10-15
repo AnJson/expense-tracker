de649b939a25368351f7ba0f3b73950bbd05acc7
Anders Jonsson <aj224rj@student.lnu.se>
2022-10-15 16:50:50 +0200

    Modify error-messages

--- src/js/model/domain/validation/Validator.js
+++ src/js/model/domain/validation/Validator.js
@@ -11,79 +11,79 @@ import { Week } from '../Week.js'
 export class Validator {
   validateExpenseTracker (model) {
     if (!(model instanceof ExpenseTracker)) {
-      throw new TypeError('The model must be an instance of ExpenseTracker.')
+      throw new TypeError('Expected the model to be of type ExpenseTracker.')
     }
   }
 
   validateWeekView (view) {
     if (!(view instanceof WeekView)) {
-      throw new TypeError('The weekView must be an instance of WeekView.')
+      throw new TypeError('Expected the weekview to be of type WeekView.')
     }
   }
 
   validateWeek (week) {
     if (!(week instanceof Week)) {
-      throw new TypeError('Week must be an instance of Week.')
+      throw new TypeError('Expected week to be of type Week.')
     }
   }
 
   validateWeekNumber (number) {
     if (!Number.isFinite(number) || (number < 1 || number > 52)) {
-      throw new TypeError('The week-number must be a number from 1 to 52.')
+      throw new TypeError('Expected the weeknumber to be a number from 1 to 52.')
     }
   }
 
   validateDay (day) {
     if (!(day instanceof Day)) {
-      throw new TypeError('Day must be an instance of Day.')
+      throw new TypeError('Expected day to be of type Day.')
     }
   }
 
   validateDayNumber (number) {
     if (!Number.isFinite(number) || (number < 1 || number > 31)) {
-      throw new TypeError('The day-number must be a number from 1 to 31.')
+      throw new TypeError('Expected daynumber to be a number from 1 to 31.')
     }
   }
 
   validateDayName (name) {
     if (!Object.values(DayName).includes(name)) {
-      throw new TypeError('The day-name must a value from the DayName-enum.')
+      throw new TypeError('Expected dayname to be a value from the DayName-enum.')
     }
   }
 
   validateCategory (category) {
     if (!(category instanceof Category)) {
-      throw new TypeError('Category must be of type Category.')
+      throw new TypeError('Expected category to be of type Category.')
     }
   }
 
   validateCategories (categories) {
     if (categories.length !== 0 && !(categories.every(category => category instanceof Category))) {
-      throw new TypeError('The categories to set must be an array containing only instances of Category.')
+      throw new TypeError('Expected categories to be an array containing only instances of Category.')
     }
   }
 
   validateExpense (expense) {
     if (!(expense instanceof Expense)) {
-      throw new TypeError('Added expense must be of type Expense.')
+      throw new TypeError('Expected expense to be of type Expense.')
     }
   }
 
   validateExpenseList (list) {
     if (!(list instanceof ExpenseList)) {
-      throw new TypeError('List of expenses must be an instance of ExpenseList.')
+      throw new TypeError('Expected list of expenses to be of type ExpenseList.')
     }
   }
 
   validateCost (cost) {
     if (!(cost instanceof Cost)) {
-      throw new TypeError('Cost that is an instance of Cost.')
+      throw new TypeError('Expected a cost to be of type Cost.')
     }
   }
 
   validateCostValue (value) {
     if (!Number.isFinite(value) || value < 0) {
-      throw new TypeError('The cost must have a value that is a number greater than or equal to zero.')
+      throw new TypeError('Expected the cost-value to be a number greater than or equal to 0.')
     }
   }
 }
